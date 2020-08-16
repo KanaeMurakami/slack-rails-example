b1528fdd65371eb38cc4ce2ad79a2317224913fe
murakami <35243965+KanaeMurakami@users.noreply.github.com>
2020-08-16 23:42:06 +0900

    完成版
    - トークンを読み込むように修正
    - helloアクション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def hello
+    client = Slack::Web::Client.new
+    client.chat_postMessage(
+      channel: '#テスト用チャンネル',
+      text: 'こんにちは'
+    )
+  end
 end
